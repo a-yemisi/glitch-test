9629b7848402d8cf1f58aeb3dacbf20e1b88e415
a-yemisi <44143580+a-yemisi@users.noreply.github.com>
2023-03-06 13:26:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, request
 app = Flask(__name__)
  
-@app.route('/receive_msg', methods=['POST','GET'])
+@app.route('/')
 def webhook():
-   if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-       if not request.args.get("hub.verify_token")== "A11thest@rs":
-           return "Verification token missmatch", 403
-       return request.args['hub.challenge'], 200
    return "Hello world", 200
  
 if __name__ == "__main__":
